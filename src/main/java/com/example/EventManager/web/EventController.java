ac324fdfd5c3e64b3bf50aa90698a961c57f30fa
spencer thiessen <spencer.thiessen98@hotmail.com>
2020-04-30 16:06:21 -0700

    Added REST api endpoints for event and attendee repos

--- src/main/java/com/example/EventManager/web/EventController.java
+++ src/main/java/com/example/EventManager/web/EventController.java
@@ -1,6 +1,7 @@
 package com.example.EventManager.web;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.EventManager.domain.Attendee;
 import com.example.EventManager.domain.AttendeeRepository;
@@ -53,5 +55,32 @@ public class EventController {
     	model.addAttribute("event", Erepository.findById(eventId));
     	return "editEvent";
     }
+    
+    /*
+     *  RESTful API / START
+     */
+    
+    @RequestMapping(value="/events", method = RequestMethod.GET)
+    public @ResponseBody List<Event> eventListRest(){
+    	return (List<Event>) Erepository.findAll();
+    }
+    
+    @RequestMapping(value="/eventId/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Event> findEventByIdRest(@PathVariable("id") Long id){
+    	return Erepository.findById(id);
+    }
+    
+    @RequestMapping(value="/attendees", method = RequestMethod.GET)
+    public @ResponseBody List<Attendee> attendeeListRest(){
+    	return (List<Attendee>) Arepository.findAll();
+    }
+    
+    @RequestMapping(value="/attendeeId/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Attendee> findAttendeeByIdRest(@PathVariable("id") Long id){
+    	return Arepository.findById(id);
+    }
+    /*
+     *  RESTful API / END
+     */
 	
 }
