ace212b6e5994431d7bef9d024d641d5d25d6420
spencer thiessen <spencer.thiessen98@hotmail.com>
2020-05-04 15:20:31 -0700

    Added working tab for 'my events'. Have to figure out how to pull username of current user still

--- src/main/java/com/example/EventManager/domain/AttendeeRepository.java
+++ src/main/java/com/example/EventManager/domain/AttendeeRepository.java
@@ -6,6 +6,6 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AttendeeRepository extends CrudRepository<Attendee, Long>{
-	Attendee findByLastName(String lastName);
+	Attendee findByUsername(String lastName);
 }
 
